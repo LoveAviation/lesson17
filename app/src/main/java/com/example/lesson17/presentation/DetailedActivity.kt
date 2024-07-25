2abada33ce534f09756504232d7c6c284ec4aa10
LoveAviation <ivangeorgmarcov@gmail.com>
2024-07-25 18:30:00 +0300

    ready?

--- app/src/main/java/com/example/lesson17/presentation/DetailedActivity.kt
+++ app/src/main/java/com/example/lesson17/presentation/DetailedActivity.kt
@@ -2,10 +2,11 @@ package com.example.lesson17.presentation
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-
 import com.example.lesson17.databinding.ActivityDetailedBinding
+
 
 class DetailedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailedBinding
@@ -15,6 +16,11 @@ class DetailedActivity : AppCompatActivity() {
         binding = ActivityDetailedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         Glide.with(this)
             .load(intent.getStringExtra("img_src"))
             .into(binding.image)
@@ -23,5 +29,12 @@ class DetailedActivity : AppCompatActivity() {
         binding.sol.text = "Sol: ${intent.getStringExtra("sol")}"
         binding.earthDate.text = "Earth date: ${intent.getStringExtra("earth_date")}"
         binding.cameraName.text = "Camera: ${intent.getStringExtra("camera")}"
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
